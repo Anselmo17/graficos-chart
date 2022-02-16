57b964f0706eba3fe0435844642ec3f05f863a12
Anselmo <ansinftec17@gmail.com>
2022-02-16 10:14:00 -0300

    Adicionando arquivos iniciais

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Documetação para os gráficos
+https://www.chartjs.org/docs/latest/samples/other-charts/doughnut.html
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,146 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'graficos';
+
+  @ViewChild("pizza", { static: true }) pizza: ElementRef;
+  @ViewChild("line", { static: true }) line: ElementRef;
+
+ meses: any = [ 
+   {
+    label:"Janeiro",
+    data: [85, 72, 86, 81, 84, 86, 94, 60, 62, 65, 41, 58],
+    borderColor: '#00AEFF',
+    fill: false
+  },
+  {
+    label:"Fevereiro",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC00',
+    fill: false
+  }
+  ,
+  {
+    label:"Março",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC00',
+    fill: false
+  },
+  {
+    label:"Abril",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC00',
+    fill: false
+  },
+  {
+    label:"Maio",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC00',
+    fill: false
+  },
+  {
+    label:"Junho",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC00',
+    fill: false
+  },
+  {
+    label:"Julho",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  },
+  {
+    label:"Agosto",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  },
+  {
+    label:"Setembro",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  },
+  {
+    label:"Outubro",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  },
+  {
+    label:"Novembro",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  },
+  {
+    label:"Dezembro",
+    data: [33, 38, 10, 93, 68, 50, 35, 29, 34, 2, 62, 4],
+    borderColor: '#FFCC55',
+    fill: false
+  }
+];
+
+
+  /* Configuração do gráfico */
+  configLine: any = {
+    type: 'line',
+    data: {
+      labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+      datasets: this.meses
+    },
+    options: {}
+  }
+
+  // Config Pizza
+  configPizza: any = {
+    data: {
+      labels: [
+        'Vendas',
+        'Pendentes',
+        'Devolução',
+        'Promoções'
+      ],
+      datasets: [{
+        label: 'Vendas da empresa',
+        data: [60, 10, 20, 10],
+        backgroundColor: [
+          'rgb(37, 168, 65)',
+          'rgb(207, 219, 30)',
+          'rgb(231, 29, 46)',
+          'rgb(175, 26, 168)'
+        ],
+        hoverOffset: 4
+      }]
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initGraficos();
+  }
+
+  initGraficos() {
+    // pizza
+    new Chart(this.pizza.nativeElement, {
+      type: 'doughnut',
+      data: this.configPizza.data,
+      options: {}
+    });
+
+    // line
+    new Chart(this.line.nativeElement, this.configLine);
+  }
 }
+
